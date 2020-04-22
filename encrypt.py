0e9b772e7f9ca7dbea710b3b61124283ee2bb531
akku333 <47693955+akku333@users.noreply.github.com>
2020-04-22 18:19:57 +0100

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 20 17:43:16 2020
+Created on Tue Apr 21 16:17:25 2020
 
-@author: neerajkulshrestha
+@author: varma
 """
 
 import json
@@ -12,16 +11,29 @@ import os
 
 
 os.getcwd()
-with open("sample.json", 'r') as f:
+with open("/Users/neerajkulshrestha/github/MyFirstProject/sample.json", 'r') as f:
     jsonString = f.read()
- 
+
 print('JSON input (sample.json):')
 print(jsonString)
- 
+
 xmlString = xmltodict.unparse(json.loads(jsonString), pretty=True)
- 
+
 print('\nXML output(output.xml):')
 print(xmlString)
- 
+
 with open('output.xml', 'w') as f:
     f.write(xmlString)
+
+import pyAesCrypt
+# encryption/decryption buffer size
+bufferSize = 64 * 1024
+password = "secret"
+# encryption of file output.xml
+
+pyAesCrypt.encryptFile("output.xml", "enoutput.xml.aes", password, bufferSize)
+print('\n----------------------------------------------------------------------')
+print('\nEncrypted file is saved in your system with name -  "enoutput.xml.aes"')
+print('\n----------------------------------------------------------------------')
+
+
